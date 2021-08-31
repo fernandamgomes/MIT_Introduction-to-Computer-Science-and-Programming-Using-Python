9b4cb02cece88cc83cfe9b39cb650ea707d23f31
Fernanda Mendes <89420917+fernandamgomes@users.noreply.github.com>
2021-08-31 16:54:59 -0300

    Update debt_one.py

--- pset2/debt_one.py
+++ pset2/debt_one.py
@@ -2,11 +2,8 @@
 Write a program to calculate the credit card balance after one year if a person only pays the minimum monthly payment required by the credit card company each month.
 
 The following variables contain values as described below:
-
     - balance - the outstanding balance on the credit card
-
     - annualInterestRate - annual interest rate as a decimal
-
     - monthlyPaymentRate - minimum monthly payment rate as a decimal
 
 For each month, calculate statements on the monthly payment and remaining balance. 
