78298230154b85afd1b4b2a1b83602b60b710a95
Fernanda Mendes <89420917+fernandamgomes@users.noreply.github.com>
2021-08-31 16:38:53 -0300

    Create bob.py

---
+++ pset1/bob.py
@@ -0,0 +1,15 @@
+'''
+Assume s is a string of lower case characters.
+
+Write a program that prints the number of times the string 'bob' occurs in s.
+
+For example, if s = 'azcbobobegghakl', then your program should print:
+
+"Number of times bob occurs is: 2"
+'''
+word = "bob"
+count = 0
+for i in range (len(s) - 2):
+    if s[i] == "b" and s[i+1] == "o" and s[i+2] == "b":
+        count = count + 1
+print("Number of times bob occurs is: " + str(count))
